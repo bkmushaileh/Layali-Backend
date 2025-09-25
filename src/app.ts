8689601011e60b54c0cdf9e05edb471c79f3788d
abdullah <aboodk841@gmail.com>
2025-09-25 22:35:04 +0300

    add all vendor crud

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,20 @@ import { errorHandler } from "./Middleware/errorHandler";
 import path from "path";
 import { notFound } from "./Middleware/notFound";
 import authRouter from "./API/Auth/auth.routers";
+import vendorRouter from "./API/Vendor/vendor.routers";
 
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true })); // <-- for form data
 
 app.use("/api/auth", authRouter);
+app.use("/api/vendor", vendorRouter);
 
 app.use(errorHandler);
 app.use(notFound);
